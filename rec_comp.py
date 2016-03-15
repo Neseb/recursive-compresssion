97e738ee592c7f28f8a063d64029e3b500cbae79
Vincent Maioli <maioli@crans.org>
2016-03-15 18:24:57 +0000

    Reworked the logic (explores the sub-trees in folders being compresssed). Seems to be working now.

--- rec_comp.py
+++ rec_comp.py
@@ -1,28 +1,71 @@
 #!/usr/bin/python
+
+#import sys
 import os
-import shutil
+import zipfile
+
+op = os.path
+
+limit = 5
 
 # Python pseudocode
-def rec_comp(file,old_root,new_root):
-	"Recursively copy a file tree, compresssing the folder if it has more than 10 files inside"
-	file_name = os.trim(file,old_root)
-	new_file = os.path.join(new_root,file_name)
-	if os.path.isdir(file):
-		os.mkdir(new_file)
-		files = os.listdir(file)
-		n = len(files)
-		if n > 10:
-			archive_name = os.path.join(new_file ,file_name)
-			shutil.make_archive(archive_name, 'zip', ("\\" + file_name + ".zip"))
-			# return 
-		else:
-			for f in files:
-				rec_comp(f,file,new_file)
-                         # return 
-	else:
-		os.system('robocopy "%s" "%s"' % (file, new_file))
-            		#return 
+def rec_comp(file,new_root):
+    """Recursively copy a file tree, compresssing the files in the folder if it 
+    has more than /limit/ non-folder files inside.
+    """
+    (old_root,file_name) = op.split(file)
+    new_file = op.join(new_root,file_name)
+
+    if op.isdir(file):
+        os.mkdir(new_file)
+        folder_content = [op.join(file,f) for f in os.listdir(file)]
+        subfiles = [f for f in folder_content if not op.isdir(f)]
+        subfolders = [f for f in folder_content if op.isdir(f)]    
+        n = len(subfiles)
+        if n > limit:
+            archive_name = op.join(new_file, file_name) + ".zip"
+            zipf = zipfile.ZipFile(archive_name, 'w', zipfile.ZIP_DEFLATED)
+            for f in subfiles:
+                zipf.write(f,op.split(f)[1]) #Name in archive is without directories
+            zipf.close()               
+            sub_iter = subfolders
+        else:
+            sub_iter = folder_content            
+
+        for f in sub_iter:
+           rec_comp(f,new_file)
+
+    else:
+        os.system('robocopy "%s" "%s" "%s"' %(old_root, new_root, file_name))
+
+
+
+if __name__ == '__main__':
+    #rec_comp(str(sys.argv[1]), str(sys.argv[2]))
+    source = r"C:\Users\Vincent\Documents\mess"
+    destination = r"C:\Users\Vincent\Documents\new1"
+    rec_comp(source,destination)
+
+        #return 
+#else:
+#    return ""
 ####################################################################
+
+### 
+##!/usr/bin/env python
+#import os
+#import zipfile
+#
+#def zipdir(path, ziph):
+#    # ziph is zipfile handle
+#    for root, dirs, files in os.walk(path):
+#        for file in files:
+#            ziph.write(os.path.join(root, file))
+#
+#if __name__ == '__main__':
+#    zipf = zipfile.ZipFile('Python.zip', 'w', zipfile.ZIP_DEFLATED)
+#    zipdir('tmp/', zipf)
+#    zipf.close()
 
 # Random pseudocode
 # file_name = trim(file,old_root)
